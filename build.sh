afe1e5dca5c6fbb60fcecd252da556248975e4e7
Ivan Buetler <ivan.buetler@compass-security.com>
2020-12-22 09:46:12 +0100

    update

---
+++ build.sh
@@ -0,0 +1,5 @@
+
+docker build --no-cache -t hackinglab/alpine-nginx-nodejs:3.2.0 -t hackinglab/alpine-nginx-nodejs:3.2 -t hackinglab/alpine-nginx-nodejs:latest -f Dockerfile .
+
+#docker push hackinglab/alpine-nginx-nodejs
+
